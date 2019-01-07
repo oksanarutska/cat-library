920a4256874acf6b29cab57092f7f0e9c4856c2f
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-01-07 21:02:54 +0200

    changed

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
